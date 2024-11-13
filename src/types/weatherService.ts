c546e65b97857f3447666551ec00166bdff5fb55
estebangccoder <estebangccoder@gmail.com>
2024-11-13 18:32:35 -0500

    Fix/ It´s repared problem with gallery and with the weather

--- src/types/weatherService.ts
+++ src/types/weatherService.ts
@@ -2,8 +2,8 @@
 const apiKey = process.env.API_KEY_WEATHER;
 export const fetchWeatherData = async (lat: number | undefined, lon: number | undefined) => {
     try {
-        console.log('Fetching weather data for:', lat, lon);
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`);
+       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`);
+       console.log(response);
         const data = await response.json();
         console.log('Weather data response:', data);
         return data;
